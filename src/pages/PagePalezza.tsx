c3de276d372aefea2736674b6f0f275073a8d6d4
palezz <mail@example.com>
2023-11-06 16:11:33 +0100

    antipasto
    Primo
    
    Secondo
    
    Dolce

--- src/pages/PagePalezza.tsx
+++ src/pages/PagePalezza.tsx
@@ -13,6 +13,11 @@ const pagePalezza = () => {
         <p> la mia branch dedicata è indietro di qualche commit rispetto a master, come faccio a portarla avanti? Cioè, come faccio a portare le modifiche fatte su master sulla mia branch dedicata?
           usando il git pull sulla master e poi il merge
         </p>
+
+        <p>Antipasto: Zeppoline</p>
+        <p>Primo: Spaghetti ai frutti di mare</p>
+        <p>Secondo: Fritto misto</p>
+        <p>Dolce: Delizia al limone</p>
     </div>
   )
 }
