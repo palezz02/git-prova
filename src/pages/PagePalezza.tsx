a214028be0a9a9805be94e5d9faaab7fa17deaf5
palezz <mail@example.com>
2023-11-06 15:23:58 +0100

    Commit es 3

--- src/pages/PagePalezza.tsx
+++ src/pages/PagePalezza.tsx
@@ -7,6 +7,7 @@ const pagePalezza = () => {
         <p>Nome: Gabriele</p>
         <p>Cognome: Palezza</p>
         <Logo></Logo>
+        <p>Per la soluzione dell' esercizio 3 ho creato una nuova branch e ho copiato le modifiche fatte per poi resettare la master</p>
     </div>
   )
 }
